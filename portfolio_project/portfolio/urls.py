2431a364ac33b85f9c980c8d4877e04aec19c429
praveen sharma <praveensharma@praveens-MacBook-Pro.local>
2022-04-09 16:48:43 +0530

    Added new database

--- portfolio_project/portfolio/urls.py
+++ portfolio_project/portfolio/urls.py
@@ -1,4 +1,4 @@
-"""portfolio_project URL Configuration
+"""portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- portfolio_project/portfolio/wsgi.py
+++ portfolio_project/portfolio/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for portfolio_project project.
+WSGI config for portfolio project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
 
 application = get_wsgi_application()
